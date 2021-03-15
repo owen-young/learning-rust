912c1e997fa6f3ff999c77d3868d09b12f26d462
owen-young <owen.young8699@gmail.com>
2021-03-14 22:02:32 -0500

    Added error checking to "guess" and match statement to actually compare.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -7,15 +8,33 @@ fn main() {
 
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
     // random isn't part of the Rust standard library yet, so it must be in
     // a crate.
 
-    std::io::stdin()
+    loop {
+        let mut guess = String::new();
+        std::io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a number!"); 
+                continue
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    
 }
